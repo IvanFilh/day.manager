66400e995e0eb294b8c951cf6493be4d5e857720
Ivan_Ramos <ivanrbsf@gmail.com>
2023-07-14 12:19:11 -0300

    importing router to APP file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import Router from "./routing/Router";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <ColorModeSwitcher justifySelf="flex-end" />
+    <Router />
   </ChakraProvider>
 );
